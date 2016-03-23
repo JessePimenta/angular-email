2bda89701f37107c283138b2df04f11e4f85384e
Jesse Pimenta <drrrreams@gmail.com>
2016-03-23 13:54:23 -0600

    added filters and send and get mail functions

---
+++ js/main.js
@@ -0,0 +1,115 @@
+var app = angular.module("myApp", []);
+
+//when angular routes to a specific page, it instantiates the designated controller for that page
+app.config(function($routeProvider){
+  $routeProvider.when('/',{
+    templateUrl: "templates/home.html",
+    controller: 'HomeController'
+  })
+  .when('/settings', {
+    templateUrl: 'templates/settings.html',
+    controller: 'SettingsController'
+  })
+  .otherwise({redirectTo: '/'})
+});
+
+app.service('mailService', ['$http', '$q', function($http, $q) {
+  var getMail = function(){
+    return $http({
+      method: 'GET',
+      url: '/api/mail'
+    });
+  };
+
+  var sendEmail = function(mail){
+      var d = $q.defer();
+      $http({
+        method: 'POST',
+        data: mail,
+        url: '/api/send'
+      }).success(function(data,status,headers) {
+        d.resolve(data);
+      }).error(function(data,status,headers){
+        d.reject(data);
+      })
+      return d.promise;
+  }
+
+  return {
+    getMail: getMail,
+    sendEmail: sendEmail
+  }
+}])
+
+// this is what defines our home controller
+//scope is a bridge between the dom element and the variable
+app.controller('HomeController',function($scope){
+$scope.selectedMail;
+
+$scope.setSelectedMail = function(mail) {
+  $scope.selectedMail = mail;
+};
+
+$scope.isSelected = function(mail){
+  if ($scope.selectedMail) {
+    return $scope.selectedMail == mail;
+  }
+}
+});
+
+app.controller('ContentController',["$scope", "$rootScope", "mailService", function($scope, $rootScope, mailService){
+
+$scope.showingReply = false;
+$scope.reply = {};
+
+$scope.toggleReplyForm = function(){
+  $scope.showingReply = !$scope.showingReply;
+  $scope.reply = {};
+  $scope.reply.to = $scope.selectedMail.from.join(', ')
+  $scope.reply.body = $scope.selectedMail.body;
+
+}
+
+$scope.sendReply = function() {
+  $scope.showingReply = false;
+  $rootScope.loading = true;
+  mailService.sendEmail($scope.reply)
+  .then(function(status){
+    $rootScope.loading = false;
+  },function(err){
+    $rootScope.loading = false;
+  });
+}
+
+$scope.$watch('selectedMail', function(evt){
+$scope.showingReply = false;
+$scope.reply = {}
+})
+
+}]);
+app.controller('MailListingController',['$scope', 'mailService', function($scope, mailService){
+  $scope.email = [];
+
+
+  mailService.getMail()
+  .success(function(data,status,headers){
+    $scope.email = data.all;
+    console.log('success');
+
+  })
+
+  .error(function(data,status,headers){
+    $scope.email = data.all
+    console.log('error');
+  });
+}])
+
+app.controller('SettingsController',function($scope){
+$scope.settings = {
+  name: 'jesse',
+  email: "drrrreams@gmail.com"
+}
+$scope.updateSettings = function(){
+  console.log('updateSettings was called');
+}
+})
